9292e274417b0f58044e319b23f3ac4324a7ede6
Hans Cristian <hans.cristian@mii.co.id>
2023-12-28 11:09:54 +0700

    add overflow-hidden class at Card component

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const Card = ({children, containerStyle}: Props) => {
     return (
-        <div className={`bg-white rounded-md border-gray-200 border-[1px] w-fit shadow-md ${containerStyle}`}>
+        <div className={`bg-white rounded-md border-gray-200 border-[1px] w-fit shadow-md overflow-hidden ${containerStyle}`}>
             {children}
         </div>
     );
