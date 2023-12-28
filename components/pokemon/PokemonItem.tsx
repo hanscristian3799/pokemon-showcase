96486833cadd66f5fb90032916816a2eaaeaf040
Hans Cristian <hans.cristian@mii.co.id>
2023-12-28 10:00:20 +0700

    add PokemonItem component

---
+++ components/pokemon/PokemonItem.tsx
@@ -0,0 +1,27 @@
+import { Pokemon } from "@/types/pokemon";
+import Image from "next/image";
+import React from "react";
+import Card from "../ui/Card";
+
+interface Props {
+    pokemon: Pokemon;
+}
+
+const PokemonItem = ({ pokemon }: Props) => {
+    return (
+        <Card containerStyle="flex flex-col items-center cursor-pointer hover:bg-slate-50 transition-all duration-300 active:bg-slate-200 overflow-hidden">
+            <Image
+                src={pokemon.image}
+                alt={pokemon.name}
+                width={300}
+                height={300}
+                quality={25}
+            />
+            <div className="p-3 bg-main-red w-full text-center">
+                <h5 className="capitalize font-semibold text-lg text-white">{pokemon.name}</h5>
+            </div>
+        </Card>
+    );
+};
+
+export default PokemonItem;
