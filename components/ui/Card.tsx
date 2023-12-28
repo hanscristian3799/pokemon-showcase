ef115bbb67d56bf27893e7962ea0a2d6b0e41ee2
Hans Cristian <hans.cristian@mii.co.id>
2023-12-28 09:59:23 +0700

    add Card component

---
+++ components/ui/Card.tsx
@@ -0,0 +1,17 @@
+'use client'
+import React from "react";
+
+interface Props {
+    children: React.ReactNode;
+    containerStyle?: string;
+}
+
+const Card = ({children, containerStyle}: Props) => {
+    return (
+        <div className={`bg-white rounded-md border-gray-200 border-[1px] w-fit shadow-md ${containerStyle}`}>
+            {children}
+        </div>
+    );
+};
+
+export default Card;
