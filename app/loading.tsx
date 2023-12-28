b1895f8ab3cbd9720e3b7493b3639e1e64006c66
Hans Cristian <hans.cristian@mii.co.id>
2023-12-28 11:11:13 +0700

    render ListLoading component in top level page Loading component

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+'use client'
+import ListLoading from '@/components/ui/ListLoading'
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <ListLoading type="grid" count={20} />
+  )
+}
+
+export default Loading
