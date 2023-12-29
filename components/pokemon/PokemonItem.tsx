e27e2b11b0e51182b452fe0be976bdfb0cbcf4fd
Hans Cristian <hans.cristian@mii.co.id>
2023-12-29 17:35:35 +0700

    make PokemonItem a client component, and add navigation to PokemonDetail page if its clicked

--- components/pokemon/PokemonItem.tsx
+++ components/pokemon/PokemonItem.tsx
@@ -1,15 +1,24 @@
+'use client';
 import { Pokemon } from "@/types/pokemon";
 import Image from "next/image";
 import React from "react";
 import Card from "../ui/Card";
+import { useRouter } from "next/navigation";
 
 interface Props {
     pokemon: Pokemon;
 }
 
 const PokemonItem = ({ pokemon }: Props) => {
+    const router = useRouter();
+
     return (
-        <Card containerStyle="flex flex-col items-center cursor-pointer hover:bg-slate-50 transition-all duration-300 active:bg-slate-200 overflow-hidden">
+        <Card
+            containerStyle="flex flex-col items-center cursor-pointer hover:bg-slate-50 transition-all duration-300 active:bg-slate-200 overflow-hidden"
+            onClick={() => {
+                router.push(`/pokemon/${pokemon.name}`)
+            }}
+        >
             <Image
                 src={pokemon.image}
                 alt={pokemon.name}
