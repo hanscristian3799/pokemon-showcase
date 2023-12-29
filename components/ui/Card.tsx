555003a775cd6ff12bac063562f69eb6d79986d7
Hans Cristian <hans.cristian@mii.co.id>
2023-12-29 17:33:07 +0700

    add onClick props in Card component

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -1,14 +1,18 @@
-'use client'
+"use client";
 import React from "react";
 
 interface Props {
     children: React.ReactNode;
     containerStyle?: string;
+    onClick?: () => void;
 }
 
-const Card = ({children, containerStyle}: Props) => {
+const Card = ({ children, containerStyle, onClick }: Props) => {
     return (
-        <div className={`bg-white rounded-md border-gray-200 border-[1px] w-fit shadow-md overflow-hidden ${containerStyle}`}>
+        <div
+            className={`bg-white rounded-md border-gray-200 border-[1px] w-fit shadow-md overflow-hidden ${containerStyle}`}
+            onClick={onClick}
+        >
             {children}
         </div>
     );
