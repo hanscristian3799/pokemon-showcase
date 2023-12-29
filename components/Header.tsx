3d15269f1264ad9f480c24d3927acb47f25eb441
Hans Cristian <hans.cristian@mii.co.id>
2023-12-29 17:34:07 +0700

    add href value to Link component in Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,10 +5,10 @@ import React from "react";
 const Header = () => {
     return (
         <div className="py-5 px-8 bg-main-red flex flex-row items-center justify-between">
-            <Link href='' className="text-xl font-semibold text-white">Pokemon Showcase</Link>
+            <Link href='/' className="text-xl font-semibold text-white">Pokemon Showcase</Link>
             <ul className="flex flex-row items-center justify-between gap-10">
                 <li>
-                    <Link href='' className="text-base text-white font-normal hover:font-semibold transition-all">Home</Link>
+                    <Link href='/' className="text-base text-white font-normal hover:font-semibold transition-all">Home</Link>
                 </li>
                 <li>
                     <Link href='' className="text-base text-white font-normal hover:font-semibold transition-all">About</Link>
