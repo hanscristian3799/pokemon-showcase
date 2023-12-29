11c1626b9e2e78547b3fbf17294c4fc75022ae2c
Hans Cristian <hans.cristian@mii.co.id>
2023-12-29 17:34:41 +0700

    add PokemonDetailStat and PokemonDetail interface

--- types/pokemon/index.ts
+++ types/pokemon/index.ts
@@ -12,3 +12,20 @@ export interface PokemonsResponse {
     count: number;
     results: PokemonResponseResult[];
 }
+
+interface PokemonDetailStat {
+    base_stat: number;
+    stat_name: 'hp' | 'attack' | 'defense' | 'special-attack' | 'special-defense' | 'speed' | 'accuracy' | 'evasion';
+}
+
+export interface PokemonDetail {
+    id: number;
+    weight: number;
+    name: string;
+    height: number;
+    base_experience: number;
+    stats: PokemonDetailStat[];
+    abilities: string[];
+    held_items: string[];
+    moves: string[];
+}
