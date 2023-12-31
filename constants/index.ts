e0705755b9373b18a894733cf8a7d1e254feb4f6
Hans Cristian <hans.cristian@mii.co.id>
2023-12-31 12:32:33 +0700

    add db name and faq collection constants

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,4 @@
 export const API_URL = 'https://pokeapi.co/api/v2';
 export const IMAGE_API_URL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork';
+export const DB_NAME = 'pokemon-showcase';
+export const FAQ_COLLECTION = 'faq';
