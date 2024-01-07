f0545be73123da8521e42c38496acdc97d879109
Hans Cristian <hans.cristian@mii.co.id>
2024-01-07 10:29:48 +0700

    add button to PokemonFIlter page in Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import PokemonList from "@/components/pokemon/PokemonList";
 import { Suspense } from "react";
 import Loading from "./loading";
+import Link from "next/link";
 
 const Home = () => {
     return (
         <>
+            <div className="flex flex-row items-center justify-end mt-8 mx-8">
+                <Link
+                    className="bg-main-red p-4 rounded-md cursor-pointer transition-all duration-300 active:opacity-80 text-white"
+                    href={`/pokemon/pikachu/ability/moves/games/forms`}
+                >
+                    Pokemon Filter
+                </Link>
+            </div>
             <Suspense fallback={<Loading />}>
                 <PokemonList />
             </Suspense>
